d4d07f0776f13fcca6d69044449873af8d436f65
DmitryMok <mdm3@yandex.ru>
2022-12-18 21:35:53 +0300

    рабочая версия до комментариев

--- src/detector.py
+++ src/detector.py
@@ -1,52 +1,89 @@
 # классы и методы распознавания объектов
 import os
 import torch
-import numpy as np
+# import numpy as np
+import yaml
+import sys
 
-class DetectorYolo():
+class _Model:
+    def __init__(self, weights, classes, description, menu_name):
+        self.weights = weights
+        self.classes = classes
+        self.description = description
+        self.menu_name = menu_name
+        self.picts = []
+        self.cmap = []
 
-    # constructor
+
+class DetectorYolo:
+    # класс для загрузки конфигурации и весов моделей
     def __init__(self):
         # загрузка моделей
-        self.loaded = False     # статус загрузки моделей
-        self.wdir = './weights' # папка с весами yolo
-        self.models = []        # здесь будут загруженные модели
+        self.loaded = False  # статус загрузки моделей
+        self.wdir = '.\weights'  # папка с весами yolo
+        self.models = []  # здесь будут загруженные модели
 
     # загружаем веса yolo из папки self.wdir = './weights'
-    def load_weights(self, models_config):
-        self.config_file = models_config 	# не используется, в будущем классы модели будут загружаться из файла (ToDo)
-        # self.models_config = {'heridal_070922.pt':{0: 'person'}}    # только одна модель для тестов, чтобы быстрее загружалась
-        self.models_config = {'heridal_070922.pt':{0: 'person'},
-                              'land_full_270922.pt':{0:'man', 1:'car', 2:'car', 3:'bike'},
-                              'mil_111022.pt':{0: 'arms', 1: 'arms', 2: 'transport', 3: 'equip', 4: 'man',
-                                               5: 'plane', 6: 'warship', 7: 'boat', 8: 'plane'},
-                              'VisBig_270922.pt':{0:'man', 1:'car', 2:'car', 3:'bike'}}
-
-        self.class_filter = [[],[0,1,2],[0,1,2,3,4],[]]   # какие классы фильтруем (оставляем)
-        # self.class_filter = [torch.tensor(f) for f in self.class_filter]
-        self.weights = list(self.models_config.keys())
-        self.class_names = [value for key, value in self.models_config.items()]
-
-        # Проверяем наличие файлов с весами и загружаем
-        for i, wfile in enumerate(self.weights):
-            path = os.path.join(self.wdir,self.weights[i])
-            if os.path.exists(path):
-                print(f'Файл {wfile} существует, загружаем')
+    def loadWeights(self, config_file='weights.yaml', progress=False):
+        self.config_file = config_file  #
+        maxnm = 3   # максимальное количество загружаемых моделей
+        try:
+            self.loadConfig()
+        except:
+            print(f'Проверьте файл конфигурации {os.path.abspath(os.path.join(self.wdir, self.config_file))}')
+            # sys.exit()
+            return 'exit'
+        # загружаем по очереди файлы с весами
+        for i, mdl in enumerate(self.models):
+            path = os.path.join(self.wdir, mdl.weights)
+            if os.path.exists(path) and i <= maxnm:  # добавляем условие для загрузки не больше трех моделей
+                print(f'Файл {mdl.weights} существует, загружаем')
                 model = torch.hub.load('yolov5-master', 'custom',
                                        path=path,
                                        source='local')
-                self.models.append(model)
+                mdl.model = model
+                # self.models.append(model)
+                if progress:  progress(int(90/min(len(mdl.weights),maxnm)*i+10))
             else:
-                print(f'Файл {wfile} не существует, пропускаем')
-        self.loaded = True	# флаг, что модели загрузились
+                print(f'Файл {mdl.weights} не существует, пропускаем')
+                del self.models[i]
+        self.loaded = True  # флаг, что модели загрузились
+        print('Результат загрузки моделей:')
+        print(f'из списка {[k["weights"] for k in self.config]} \nзагружены {[k.weights for k in self.models]}')
+        return [k.menu_name for k in self.models]
 
-        # return self.models
+    # загружаем параметры конфигурации из файла self.wdir = './weights/weights.yaml'
+    def loadConfig(self):
+        with open(os.path.join(self.wdir, self.config_file)) as fh:
+            read_data = yaml.load(fh, Loader=yaml.FullLoader)
+        # парсер конфигурационного файла
+        for k in read_data:
+            # print(k.keys())
+            model = _Model(k['weights'], k['classes'], k['description'], k['menu_name'])
+            # print(k['name'])
+            if 'picts' in k.keys():
+                model.picts = k['picts']
+            else:
+                model.picts = []
+            if 'cmap' in k.keys():
+                # model.cmap = k['cmap']
+                for p in k['classes'].keys():
+                    h = k['cmap'][p].lstrip('#')
+                    model.cmap.append(tuple(int(h[i:i + 2], 16) for i in (4, 2, 0)))
+                else: model.cmap = []
+            self.models.append(model)   # массив моделей
 
-	# делаем распознавание
-    def detect(self, image, model_index=0):
-        res = self.models[model_index](image)
+        # self.weights = [k['name'] for k in read_data]
+        self.config = read_data  # сохраняем, чтобы брать описание и т.п.
+
+    # делаем распознавание (на вход изображение и индекс модели, возвращает массив с координатами рамок)
+    def getDetect(self, image, model_index=0):
+        res = self.models[model_index].model(image)
         # print(self.class_filter)
         # if len(self.class_filter[model_index]):
         #     res = res[[torch.isin(res[...,4], self.class_filter[model_index])]]
+        # print('xywh1')
+        # print('xywh', res.xywh[0])
+        # print('xywh1')
         return res.xyxy[0].cpu().detach().numpy()
         # return res.xyxy[0]
---
+++ src/visualizer.py
@@ -0,0 +1,104 @@
+# классы и методы визуализации объектов
+import os.path
+
+import cv2
+import numpy as np
+
+def mean(a,b):
+  return (a+b)//4
+
+class Annotator:
+    # визуализирует результаты распознавания
+    def __init__(self):
+        self.lw = 1  # lтолщина линии
+        self.pict_list = [] # массив пиктограмм
+        self.mask_list = [] # массив масок пиктограмм
+        self.cmap_default = [(0,0,220), (0,150,0),
+                             (0,150,0), (200,0,0),
+                             (0,0,150), (0,200,200),
+                             (0,0,200), (0,200,0),
+                             (200,0,0), (200,200,0),
+                             (0,0,150), (0,200,200),
+                             (0,0,200), (0,200,0),
+                             ]
+        self.cmap_list = self.cmap_default.copy() # массив RGB цвета каждого класса
+        self.sc = 4     # масштаб текста (чем больше, тем мельче)
+
+    # если в функцию передано изображение, работаем с ним (для исключений)
+    def setImage(self, im):
+        if hasattr(im, "__len__"):  # если массив, значит передано изображение и работаем с ним
+            self.im = im
+            # print('if!!!')
+            assert self.im.data.contiguous, 'Image not contiguous'
+
+    # визуализирует реузльтаты распознавания (массив bouding boxes)
+    def drawDetections(self, im, bboxes, bbox_type=1, lw=1, picts = False, conf_text = False):
+        self.setImage(im)
+        self.lw = lw
+        self.picts = picts              # выводим пиктограммы
+        self.conf_text = conf_text      # выводим уверенность
+        for bbox in bboxes:
+            self.drawBbox(bbox[:], bbox_type=bbox_type, color=self.cmap_list[int(bbox[-1])])
+        return self.im
+
+    # визуализирует bounding box
+    def drawBbox(self, box, bbox_type=1, color=(128,128,128)):
+        p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
+        w, h = int(p2[0] - p1[0]), int(p2[1] - p1[1])
+        dc = 5  # смещение пиктограммы от угла
+        if bbox_type == 1:      # рамка
+            cv2.rectangle(self.im, p1, p2, color=color, thickness=self.lw, lineType=cv2.LINE_AA)
+        elif bbox_type == 2:    # окружность
+            x, y = p1[0] + w // 2, p1[1] + h // 2
+            cv2.circle(self.im, (x,y), h//2, (0, 255, 0), 1)
+        elif bbox_type == 3:    # скобки
+            ow = min(w,h)//4   # offset width
+            # выводим рамку
+            pts = np.array([[p1[0]+ow, p1[1]],[p1[0],p1[1]],[p1[0], p2[1]],[p1[0]+ow, p2[1]]])
+            cv2.polylines(self.im, [pts], False, color, self.lw)
+            pts = np.array([[p2[0]-ow, p2[1]],[p2[0],p2[1]],[p2[0], p1[1]],[p2[0]-ow, p1[1]]])
+            cv2.polylines(self.im, [pts], False, color, self.lw)
+        if self.picts:
+            # выводим пиктограммы
+            if self.pict_list:  # сначала обработать файлы пиктограмм функцией getPicts
+                cl = int(box[-1]) # номер класса
+                size_w = self.pict_list[cl].shape[0]
+                size_h = self.pict_list[cl].shape[1]
+                # print(-size_w + p1[0], -size_h + p1[1], 'cl=', cl)
+                if ((-size_w + p1[1]) > 0 and (-size_h + p1[0]) > 0):
+                    roi = self.im[-size_w + p1[1]:p1[1], -size_h + p1[0]:p1[0]]
+                    roi[np.where(self.mask_list[cl])] = 0
+                    roi += self.pict_list[cl]
+        if self.conf_text:
+            label = str(round(box[-2]-0.05,2))    # вероятность
+            self.sc = 4  # масштаб текста
+            w_text, h_text = cv2.getTextSize(label, 0, fontScale=self.lw / self.sc, thickness=1)[0]  # text width, height  # cv2.getTextSize(label, 0, fontScale=self.lw / 3, thickness=tf)[0]  # text width, height
+            outside = p1[0] - w_text >= 3
+            p2 = p1[0]-w_text if outside else 0, p1[1]+h_text + 2
+            cv2.rectangle(self.im, p1, p2, color, -1, cv2.LINE_AA)  # filled
+            cv2.putText(self.im,
+                        label, (p1[0]-w_text, p1[1]+h_text - 0 if outside else p1[1] + h_text + 2),
+                        0,
+                        self.lw / self.sc,
+                        (255, 255, 255),
+                        thickness=1,
+                        lineType=cv2.LINE_AA)
+
+    # загружает пиктограммы, создает массив с пиктограммой и маской
+    def setPicts(self, pict_files=None):
+        self.pict_list, self.mask_list = [], []
+        if pict_files:
+            for fname in pict_files:
+                logo = cv2.imread(os.path.join('.\img', fname))
+                # size_w = logo.shape[0]
+                # size_h = logo.shape[1]
+                # создаем маску пиктограммы
+                img2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
+                ret, mask = cv2.threshold(img2gray, 254, 255, cv2.THRESH_BINARY)
+                logo[np.where(mask)] = 0
+                self.pict_list.append(logo) # пиктограмма с альфа каналом
+                # инвертируем маску, чтобы фон имел значение 0
+                mask = mask * (-1)
+                mask += 255
+                self.mask_list.append(mask) # маска альфа канала
+
